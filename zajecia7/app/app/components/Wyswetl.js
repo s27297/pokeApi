c807fd1ccdc643ea17b7f4ed3216b75aceb8df50
s27297 <123184307+s27297@users.noreply.github.com>
2024-11-25 12:50:24 +0100

    Update Wyswetl.js

--- zajecia7/app/app/components/Wyswetl.js
+++ zajecia7/app/app/components/Wyswetl.js
@@ -4,6 +4,7 @@ import ExpanseDetails from "@/app/components/ExpanseDetails";
 import EditWydatek from "@/app/add_edit/EditWydatek";
 import {useContext, useEffect, useLayoutEffect} from "react";
 import {GlobalContext} from "@/app/Providers/WydatekProvider";
+import {goodbyeChime, welcomeChime} from "@/app/chimes/chimes";
 
 
 //zaedituj :patrz EditWydatek
@@ -32,24 +33,45 @@ export default function Wyswetl(){
     let {category,date,expanse,editowanie,list,onCategoryChange,onDateChange, ustawListePofiltrowana,listPofiltrowana}=useContext(GlobalContext)
 
 
-   if(!list)return<p>Pusta lista</p>
+    if(!list)return<p>Pusta lista</p>
     if(expanse!==-1)return (<ExpanseDetails wydatek={list.filter(q=>q.id==expanse)[0]}></ExpanseDetails> )
     if(editowanie!==-1)return (<EditWydatek  wydatek={list.filter(q=>q.id==editowanie)[0]}></EditWydatek> )
     //lab7
+
+
 
     useLayoutEffect(() => {
         async function posortuj(){
             const newList1=list.filter(wydatek=>((wydatek.category===category||category==="All")&&wydatek.date>date))
             ustawListePofiltrowana(newList1)
         }
-       posortuj()
+        posortuj()
     }, [category,date,list])
-   // const  newList=list.filter(wydatek=>((wydatek.category===category||category==="All")&&wydatek.date>date))
-   if(!listPofiltrowana.length)return<div style={{backgroundColor: "lightblue"}}><p>Pusta lista</p></div>
+    useEffect(() => {
+    
+       const interval= setInterval(()=>{
+
+            let s=0
+            list.map(wydatek=>s+=wydatek.kwota)
+
+            if(s>10000)
+                console.log("za duze wydatki, one rowne: "+s)
+           else
+               console.log("wydatki nie przekroczaja budgetu i rowne: "+s)
+            },2000
+        )
+        
+        return () => {
+            clearInterval(interval)
+
+        };
+    }, );
+    // const  newList=list.filter(wydatek=>((wydatek.category===category||category==="All")&&wydatek.date>date))
+    if(!listPofiltrowana.length)return<div style={{backgroundColor: "lightblue"}}><p>Pusta lista</p></div>
     return(
         <div style={{backgroundColor: "lightblue"}}>
             {listPofiltrowana.map(wydatek => <Wydatek key={wydatek.id}
-                                            wydatek={wydatek} >
+                                                      wydatek={wydatek} >
 
             </Wydatek>)}
             <br/>
